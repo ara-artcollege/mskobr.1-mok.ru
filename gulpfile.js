c5137fc9138a7cd5eee4d74ed4fb578c67956b59
Vladislav Golik <Vladislav.Golik@gmail.com>
2019-03-18 20:57:59 +0300

    task

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,25 @@ const   gulp = require('gulp'),
         htmlMin = require('gulp-htmlmin');
 
 
-
 gulp.task('main', () => {
     return gulp.src('app/html/page3969187.html')
-        .pipe(htmlMin({ collapseWhitespace: true }))
+        .pipe(htmlMin({
+            collapseWhitespace: true
+        }))
         .pipe(gulpRename('/main.html'))
         .pipe(gulp.dest('./'));
 });
 
 gulp.task('reception', () => {
     return gulp.src('app/html/page4396977.html')
-        .pipe(htmlMin({ collapseWhitespace: true }))
+        .pipe(htmlMin({
+            collapseWhitespace: true
+        }))
         .pipe(gulpRename('/reception.html'))
         .pipe(gulp.dest('./'));
 });
+
+
 
 
 
@@ -26,14 +31,15 @@ gulp.task('clean', () => {
     return delFiles('*.html');
 })
 
-gulp.task('w', () => {
-    return gulp.watch('app/html/*.html', gulp.series('default'))
-})
 
 gulp.task('default', gulp.series(
     'clean',
     gulp.parallel(
         'main',
         'reception',
-        )
+    )
 ));
+
+gulp.task('w', () => {
+    return gulp.watch('app/html/*.html', gulp.series('default'))
+})
