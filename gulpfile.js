e64bb2fb62721cb6d765abe13b7cb6ed46a19273
Vladislav <rozzel@mail.ru>
2019-03-19 14:49:52 +0300

    cstv

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const gulp = require('gulp'),
     htmlMin = require('gulp-htmlmin'),
     gulpReplace = require('gulp-replace');
 
+gulp.task('clean', () => {
+    return delFiles(['*.html', 'cstv']);
+});
 
 gulp.task('main', () => {
     return gulp.src('app/html/page3969187.html')
@@ -23,6 +26,7 @@ gulp.task('reception', () => {
         .pipe(gulp.dest('./'));
 });
 
+// cstv
 gulp.task('cstv:about', () => {
     return gulp.src('app/html/page4024424.html')
         .pipe(gulpReplace('="css/', '="../css/'))
@@ -51,9 +55,147 @@ gulp.task('cstv:documents', () => {
         .pipe(gulp.dest('./'));
 });
 
-gulp.task('clean', () => {
-    return delFiles(['*.html', 'cstv']);
+gulp.task('cstv:documents', () => {
+    return gulp.src('app/html/page4117307.html')
+        .pipe(gulpReplace('="css/', '="../css/'))
+        .pipe(gulpReplace('="js/', '="../js/'))
+        .pipe(gulpReplace('\"images/', '\"../images/'))
+        .pipe(gulpReplace('\'images/', '\'../images/'))
+        .pipe(gulpReplace('target="_blank"> ', 'target="_parent">'))
+        .pipe(htmlMin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulpRename('cstv/documents.html'))
+        .pipe(gulp.dest('./'));
 });
+
+gulp.task('cstv:graduate', () => {
+    return gulp.src('app/html/page4175062.html')
+        .pipe(gulpReplace('="css/', '="../css/'))
+        .pipe(gulpReplace('="js/', '="../js/'))
+        .pipe(gulpReplace('\"images/', '\"../images/'))
+        .pipe(gulpReplace('\'images/', '\'../images/'))
+        .pipe(gulpReplace('target="_blank"> ', 'target="_parent">'))
+        .pipe(htmlMin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulpRename('cstv/graduate.html'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('cstv:employer', () => {
+    return gulp.src('app/html/page4208064.html')
+        .pipe(gulpReplace('="css/', '="../css/'))
+        .pipe(gulpReplace('="js/', '="../js/'))
+        .pipe(gulpReplace('\"images/', '\"../images/'))
+        .pipe(gulpReplace('\'images/', '\'../images/'))
+        .pipe(gulpReplace('target="_blank"> ', 'target="_parent">'))
+        .pipe(htmlMin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulpRename('cstv/employer.html'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('cstv:news', () => {
+    return gulp.src('app/html/page5213876.html')
+        .pipe(gulpReplace('="css/', '="../css/'))
+        .pipe(gulpReplace('="js/', '="../js/'))
+        .pipe(gulpReplace('\"images/', '\"../images/'))
+        .pipe(gulpReplace('\'images/', '\'../images/'))
+        .pipe(gulpReplace('target="_blank"> ', 'target="_parent">'))
+        .pipe(htmlMin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulpRename('cstv/news.html'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('cstv:research', () => {
+    return gulp.src('app/html/page4198140.html')
+        .pipe(gulpReplace('="css/', '="../css/'))
+        .pipe(gulpReplace('="js/', '="../js/'))
+        .pipe(gulpReplace('\"images/', '\"../images/'))
+        .pipe(gulpReplace('\'images/', '\'../images/'))
+        .pipe(gulpReplace('target="_blank"> ', 'target="_parent">'))
+        .pipe(htmlMin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulpRename('cstv/research.html'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('cstv:success', () => {
+    return gulp.src('app/html/page4209200.html')
+        .pipe(gulpReplace('="css/', '="../css/'))
+        .pipe(gulpReplace('="js/', '="../js/'))
+        .pipe(gulpReplace('\"images/', '\"../images/'))
+        .pipe(gulpReplace('\'images/', '\'../images/'))
+        .pipe(gulpReplace('target="_blank"> ', 'target="_parent">'))
+        .pipe(htmlMin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulpRename('cstv/success.html'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('cstv:monitoring', () => {
+    return gulp.src('app/html/page4209200.html')
+        .pipe(gulpReplace('="css/', '="../css/'))
+        .pipe(gulpReplace('="js/', '="../js/'))
+        .pipe(gulpReplace('\"images/', '\"../images/'))
+        .pipe(gulpReplace('\'images/', '\'../images/'))
+        .pipe(gulpReplace('target="_blank"> ', 'target="_parent">'))
+        .pipe(htmlMin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulpRename('cstv/monitoring.html'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('cstv:photo', () => {
+    return gulp.src('app/html/page4117081.html')
+        .pipe(gulpReplace('="css/', '="../css/'))
+        .pipe(gulpReplace('="js/', '="../js/'))
+        .pipe(gulpReplace('\"images/', '\"../images/'))
+        .pipe(gulpReplace('\'images/', '\'../images/'))
+        .pipe(gulpReplace('target="_blank"> ', 'target="_parent">'))
+        .pipe(htmlMin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulpRename('cstv/photo.html'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('cstv:vuz', () => {
+    return gulp.src('app/html/page4215020.html')
+        .pipe(gulpReplace('="css/', '="../css/'))
+        .pipe(gulpReplace('="js/', '="../js/'))
+        .pipe(gulpReplace('\"images/', '\"../images/'))
+        .pipe(gulpReplace('\'images/', '\'../images/'))
+        .pipe(gulpReplace('target="_blank"> ', 'target="_parent">'))
+        .pipe(htmlMin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulpRename('cstv/vuz.html'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('cstv:feedback', () => {
+    return gulp.src('app/html/page4215461.html')
+        .pipe(gulpReplace('="css/', '="../css/'))
+        .pipe(gulpReplace('="js/', '="../js/'))
+        .pipe(gulpReplace('\"images/', '\"../images/'))
+        .pipe(gulpReplace('\'images/', '\'../images/'))
+        .pipe(gulpReplace('target="_blank"> ', 'target="_parent">'))
+        .pipe(htmlMin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulpRename('cstv/feedback.html'))
+        .pipe(gulp.dest('./'));
+});
+
+// /cstv
 
 
 gulp.task('default', gulp.series(
@@ -62,10 +204,15 @@ gulp.task('default', gulp.series(
         'main',
         'reception',
         'cstv:about',
-        'cstv:documents'
+        'cstv:documents',
+        // 'cstv:graduate',
+        // 'cstv:employer',
+        // 'cstv:news',
+        // 'cstv:research',
+        // 'cstv:success',
+        // 'cstv:monitoring',
+        // 'cstv:photo',
+        // 'cstv:vuz',
+        // 'cstv:feedback',
     )
 ));
-
-gulp.task('w', () => {
-    return gulp.watch('app/html/*.html', gulp.series('default'))
-})
