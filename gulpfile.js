29d883a1f47069f02d0824558b8086bd7dd86d49
Vladislav <rozzel@mail.ru>
2019-03-22 13:28:00 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 const gulp = require('gulp'),
-    gulpRename = require('gulp-rename'),
     delFiles = require('del'),
-    htmlMin = require('gulp-htmlmin'),
     download = require('gulp-download-files'),
     gulpReplace = require('gulp-replace');
 
@@ -14,34 +12,30 @@ gulp.task('clean:tilda', () => {
 
 gulp.task('download:tilda', () => {
     return download({
-                    'reception.html':       'http://mskobr.tilda.ws/reception/',
-                    'main.html':            'http://mskobr.tilda.ws/main/',
-                    'pervoklassnik.html':   'http://mskobr.tilda.ws/pervoklassnik/',
-                    'cstv/about.html': 'http://mskobr.tilda.ws/cstv/about/',
-                    'page5213876.html': 'http://mskobr.tilda.ws/cstv/news/',
-                    'cstv/news.html': 'http://mskobr.tilda.ws/cstv/documents/',
-                    'cstv/documents.html': 'http://mskobr.tilda.ws/cstv/graduate/',
-                    'cstv/employer.html': 'http://mskobr.tilda.ws/cstv/employer/',
-                    'cstv/competences.html': 'http://mskobr.tilda.ws/cstv/competences/',
-                    'cstv/research.html': 'http://mskobr.tilda.ws/cstv/research/',
-                    'cstv/success.html': 'http://mskobr.tilda.ws/cstv/success/',
-                    'cstv/monitoring.html': 'http://mskobr.tilda.ws/cstv/monitoring/',
-                    'cstv/photo.html': 'http://mskobr.tilda.ws/cstv/photo/',
-                    'cstv/vuz.html': 'http://mskobr.tilda.ws/cstv/vuz/',
-                    'cstv/archive.html': 'http://mskobr.tilda.ws/cstv/archive/',
-                    'cstv/feedback.html': 'http://mskobr.tilda.ws/cstv/feedback/',
-                    'cstv/menu.html': 'http://mskobr.tilda.ws/cstv/menu/'
+                    'reception.html':           'http://mskobr.tilda.ws/reception/',
+                    'main.html':                'http://mskobr.tilda.ws/main/',
+                    'pervoklassnik.html':       'http://mskobr.tilda.ws/pervoklassnik/',
+                    'cstv/about.html':          'http://mskobr.tilda.ws/cstv/about/',
+                    'cstv/news.html':           'http://mskobr.tilda.ws/cstv/news/',
+                    'cstv/documents.html':      'http://mskobr.tilda.ws/cstv/documents/',
+                    'cstv/graduate.html':       'http://mskobr.tilda.ws/cstv/graduate/',
+                    'cstv/employer.html':       'http://mskobr.tilda.ws/cstv/employer/',
+                    'cstv/competences.html':    'http://mskobr.tilda.ws/cstv/competences/',
+                    'cstv/research.html':       'http://mskobr.tilda.ws/cstv/research/',
+                    'cstv/success.html':        'http://mskobr.tilda.ws/cstv/success/',
+                    'cstv/monitoring.html':     'http://mskobr.tilda.ws/cstv/monitoring/',
+                    'cstv/photo.html':          'http://mskobr.tilda.ws/cstv/photo/',
+                    'cstv/vuz.html':            'http://mskobr.tilda.ws/cstv/vuz/',
+                    'cstv/archive.html':        'http://mskobr.tilda.ws/cstv/archive/',
+                    'cstv/feedback.html':       'http://mskobr.tilda.ws/cstv/feedback/',
+                    'cstv/menu.html':           'http://mskobr.tilda.ws/cstv/menu/'
 
                     })
         .pipe(gulp.dest('tilda'))
 });
 
 gulp.task('main', () => {
-    return gulp.src([
-            'tilda/main.html',
-            'tilda/reception.html',
-            'tilda/pervoklassnik.html'
-        ])
+    return gulp.src('tilda/*.html')
         .pipe(gulp.dest('./'))
 });
 
