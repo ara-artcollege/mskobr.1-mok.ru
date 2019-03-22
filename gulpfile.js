c8717229faf8978a76d0c8b1dcdf845e64123685
Vladislav <rozzel@mail.ru>
2019-03-22 13:17:24 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -16,21 +16,22 @@ gulp.task('download:tilda', () => {
     return download({
                     'reception.html':       'http://mskobr.tilda.ws/reception/',
                     'main.html':            'http://mskobr.tilda.ws/main/',
-                    'page4024424.html': 'http://mskobr.tilda.ws/cstv/about/',
+                    'pervoklassnik.html':   'http://mskobr.tilda.ws/pervoklassnik/',
+                    'cstv/about.html': 'http://mskobr.tilda.ws/cstv/about/',
                     'page5213876.html': 'http://mskobr.tilda.ws/cstv/news/',
-                    'page4117307.html': 'http://mskobr.tilda.ws/cstv/documents/',
-                    'page4175062.html': 'http://mskobr.tilda.ws/cstv/graduate/',
-                    'page4208064.html': 'http://mskobr.tilda.ws/cstv/employer/',
-                    'page4209734.html': 'http://mskobr.tilda.ws/cstv/competences/',
-                    'page4198140.html': 'http://mskobr.tilda.ws/cstv/research/',
-                    'page4209200.html': 'http://mskobr.tilda.ws/cstv/success/',
-                    'page4211252.html': 'http://mskobr.tilda.ws/cstv/monitoring/',
-                    'page4117081.html': 'http://mskobr.tilda.ws/cstv/photo/',
-                    'page4215020.html': 'http://mskobr.tilda.ws/cstv/vuz/',
-                    'page4216643.html': 'http://mskobr.tilda.ws/cstv/archive/',
-                    'page4215461.html': 'http://mskobr.tilda.ws/cstv/feedback/',
-                    'page4240226.html': 'http://mskobr.tilda.ws/cstv/menu/',
-                    'pervoklassnik.html':   'http://mskobr.tilda.ws/pervoklassnik/'
+                    'cstv/news.html': 'http://mskobr.tilda.ws/cstv/documents/',
+                    'cstv/documents.html': 'http://mskobr.tilda.ws/cstv/graduate/',
+                    'cstv/employer.html': 'http://mskobr.tilda.ws/cstv/employer/',
+                    'cstv/competences.html': 'http://mskobr.tilda.ws/cstv/competences/',
+                    'cstv/research.html': 'http://mskobr.tilda.ws/cstv/research/',
+                    'cstv/success.html': 'http://mskobr.tilda.ws/cstv/success/',
+                    'cstv/monitoring.html': 'http://mskobr.tilda.ws/cstv/monitoring/',
+                    'cstv/photo.html': 'http://mskobr.tilda.ws/cstv/photo/',
+                    'cstv/vuz.html': 'http://mskobr.tilda.ws/cstv/vuz/',
+                    'cstv/archive.html': 'http://mskobr.tilda.ws/cstv/archive/',
+                    'cstv/feedback.html': 'http://mskobr.tilda.ws/cstv/feedback/',
+                    'cstv/menu.html': 'http://mskobr.tilda.ws/cstv/menu/'
+
                     })
         .pipe(gulp.dest('tilda'))
 });
@@ -41,52 +42,12 @@ gulp.task('main', () => {
             'tilda/reception.html',
             'tilda/pervoklassnik.html'
         ])
-        .pipe(htmlMin({
-            collapseWhitespace: true
-        }))
         .pipe(gulp.dest('./'))
 });
 
 gulp.task('cstv', () => {
-    return gulp.src([
-            'tilda/page4024424.html',
-            'tilda/page4117307.html',
-            'tilda/page4175062.html',
-            'tilda/page4208064.html',
-            'tilda/page5213876.html',
-            'tilda/page4198140.html',
-            'tilda/page4209200.html',
-            'tilda/page4211252.html',
-            'tilda/page4117081.html',
-            'tilda/page4215020.html',
-            'tilda/page4215461.html',
-            'tilda/page4209734.html',
-            'tilda/page4216643.html',
-            'tilda/page4240226.html'
-        ])
+    return gulp.src('tilda/cstv/*.html')
         .pipe(gulpReplace('target="_blank">~', 'target="_top">'))
-        .pipe(htmlMin({
-            collapseWhitespace: true
-        }))
-        .pipe(gulpRename(function (path) {
-            let rules = {
-                'page4024424': 'about',
-                'page4117307': 'documents',
-                'page4175062': 'graduate',
-                'page4208064': 'employer',
-                'page5213876': 'news',
-                'page4198140': 'research',
-                'page4209200': 'success',
-                'page4211252': 'monitoring',
-                'page4117081': 'photo',
-                'page4215020': 'vuz',
-                'page4215461': 'feedback',
-                'page4209734': 'competences',
-                'page4216643': 'archive',
-                'page4240226': 'menu'
-            };
-            path.basename = rules[path.basename]
-        }))
         .pipe(gulp.dest('./cstv'))
 });
 
