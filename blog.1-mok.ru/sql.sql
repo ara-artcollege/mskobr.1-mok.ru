7fcbe871ec0532a4a8c580cbe69f1e90632b1657
Vladislav <gvn@artcollege.ru>
2019-09-09 15:56:41 +0300

    blog

--- blog.1-mok.ru/sql.sql
+++
@@ -1,3 +0,0 @@
-UPDATE wp_options SET option_value = REPLACE(option_value, 'http://blog.1-mok.ru', 'https://test.1-mok.ru') WHERE option_name = 'home' OR option_name = 'siteurl';
-UPDATE wp_posts SET guid = REPLACE(guid, 'http://blog.1-mok.ru','https://test.1-mok.ru');
-UPDATE wp_posts SET post_content = REPLACE(post_content, 'http://blog.1-mok.ru', 'https://test.1-mok.ru');
